6df8180e1138f7d2e62d67d56e7c962fa6a6365c
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-15 19:55:42 +0530

    Add optional close icon to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,6 @@
-function Button({ className, onClick, type, title }) {
+import { useEffect, useState } from "react";
+
+function Button({ className, onClick, type, title, closeImg = false }) {
   return (
     <>
       <button
@@ -7,6 +9,23 @@ function Button({ className, onClick, type, title }) {
         className={`capitalize px-3 py-2 rounded-md bg-primary text-white shadow-md hover:bg-primary hover:text-white hover:shadow-md ${className}`}
       >
         {title}
+
+        {closeImg && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-3 h-3"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        )}
       </button>
     </>
   );
