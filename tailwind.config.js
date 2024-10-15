0030f125fff17e7c9b546abdd22a6bd30cf50497
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-15 19:00:31 +0530

    Enhance Tailwind CSS configuration with custom fonts, colors, and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        robotoslab: ["Roboto Slab", "serif"],
+      },
+      colors: {
+        primary: "#121C2F",
+        secondary: "#69a79c",
+        light: "#f7f7f7",
+        dark: "#333333",
+        dark2: "#999999",
+      },
+      keyframes: {
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-10%)",
+            "animation-timing-function": "cubic-bezier(0.7, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+      animation: {
+        bounce: "bounce 5s infinite",
+      },
+    },
   },
   plugins: [],
 };
